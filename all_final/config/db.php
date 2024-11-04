0613775749a1fe4cf7694e59e71fdc9bcb73ea44
TIMEMIXCELL <nattapal2547@gmail.com>
2024-11-04 21:13:11 +0700

    Update db.php

--- all_final/config/db.php
+++ all_final/config/db.php
@@ -1,28 +1,12 @@
 <?php
-class MyDB {
-    private $conn;
-
-    function __construct() {
-        $servername = "database-3.chaocgea2ln5.us-east-1.rds.amazonaws.com"; // ใส่ RDS endpoint
-        $username = "admin"; // ใส่ชื่อผู้ใช้ RDS
-        $password = "Natta123$"; // ใส่รหัสผ่าน RDS
-        $dbname = "database-3"; // ใส่ชื่อฐานข้อมูล
-
-        // สร้างการเชื่อมต่อ
-        $this->conn = new mysqli($servername, $username, $password, $dbname);
-
-        // เช็คการเชื่อมต่อ
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-        echo "Connected successfully to RDS";
-    }
-
-    function close() {
-        $this->conn->close();
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('mainDatabase.db');
     }
 }
 
-// สร้างออบเจ็กต์ของ MyDB
+// 2. Open Database 
 $db = new MyDB();
 ?>
