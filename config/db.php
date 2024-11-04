19915500e0778ae0d12a0c36d28aedeebc814795
TIMEMIXCELL <nattapal2547@gmail.com>
2024-11-04 23:43:23 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,31 +1,12 @@
 <?php
-class MyDB
+class MyDB extends SQLite3
 {
-    private $pdo;
-
-    public function __construct()
+    function __construct()
     {
-        $host = 'database-3.chaocgea2ln5.us-east-1.rds.amazonaws.com'; // เปลี่ยนให้เป็น endpoint ของ RDS
-        $dbName = 'database-3'; // ชื่อฐานข้อมูลที่ต้องการใช้
-        $user = 'admin';
-        $pass = 'Natta123$';
-
-        try {
-            $this->pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $user, $pass);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
-        }
-    }
-
-    public function getConnection()
-    {
-        return $this->pdo;
+        $this->open('mainDatabase.db');
     }
 }
 
-// เปิดการเชื่อมต่อ
+// 2. Open Database 
 $db = new MyDB();
-$connection = $db->getConnection();
-
 ?>
